5d810f2762ce31ec409c3424c07dc4a299511e22
alecmstockman <alecmstockman@gmail.com>
2026-05-22 18:04:26 -0500

    cleanup

--- src/display/sidebar.py
+++ src/display/sidebar.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from ..models.playlist import Playlist, PlaylistManager
-from ..vlc_player import VLCPlayer
+
 
 class Sidebar(ttk.Frame):
     def __init__(self, parent, library, playlist_manager):
