d82fadd1f7dfc1e44f2e233672a5b8992683e924
alecmstockman <alecmstockman@gmail.com>
2026-05-23 14:09:00 -0500

    continued playlist work

--- src/database/playlist_db.py
+++ src/database/playlist_db.py
@@ -1,13 +1,14 @@
 import sqlite3
 from pathlib import Path
+import uuid
 from ..models.track import Track
 from ..models.playlist import Playlist
 
 
-DB_PATH = Path("data/playlist.db")
+PLAYLISTS_DB_PATH = Path("data/playlists.db")
 
 def init_playlists_db():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(PLAYLISTS_DB_PATH)
     cursor = conn.cursor()
 
     try:
@@ -23,9 +24,11 @@ def init_playlists_db():
         print("Unable to create playlists_db")
         conn.close()
 
+PLAYLIST_TRACKS_DB_PATH = Path("data/playlists_tracks.db")
+
 def init_playlist_tracks_db():
     print("init playlist db")
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(PLAYLIST_TRACKS_DB_PATH)
     cursor = conn.cursor()
 
     try:
@@ -45,6 +48,23 @@ def init_playlist_tracks_db():
     except sqlite3.IntegrityError:
         print("Unable to create playlist_tracks db")
         conn.close()
+
+
+def create_playlist_in_playlists_db(name: str, playlist_id: str):
+    print(f"creating playlist: {name}")
+    conn = sqlite3.connect()
+    curser = conn.cursor()
+
+    try:
+        conn.execute("""
+            
+        """)
+        conn.commit()
+        conn.close()
+    except sqlite3.IntegrityError:
+        print(f"Unable to create playlist: {name}")
+        conn.close()
+
 
 # def save_playlist_to_playlist_db(playlist: Playlist):
 #     conn = sqlite3.connect(DB_PATH)
